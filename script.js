d5c5be36c5b4f20c272f23f1b7acd40c583d10c5
surajkumar345678 <kumarsuraj345678@gmail.com>
2025-01-12 18:34:30 +0530

    Build REST Countries API project with search, dark mode, and responsive UI

---
+++ script.js
@@ -0,0 +1,28 @@
+const countriesContainer = document.querySelector('.countries-container')
+
+
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+        // console.log(country);
+        const countryCard = document.createElement('a')
+        countryCard.classList.add('country-card')
+        countryCard.href = `/country.html?name=${country.name.common}`
+        countryCard.innerHTML = `
+            <img src="${country.flags.svg}" alt="flag" />
+            <div class="card-text">
+                <h3 class="card-title">${country.name.common}</h3>
+                <p><b>Population: </b>${Intl.NumberFormat("en-IN").format(
+                    country.population
+                  )}</p>
+                <p><b>Region: </b>${country.region}</p>
+                <p><b>Capital: </b>${country.capital?.[0]}</p>
+            </div>
+    `
+        countriesContainer.append(countryCard)
+    })
+  })
+
+
+  
