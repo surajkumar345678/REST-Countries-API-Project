cc44fd03e53cc7216d1e5aa6e6a38a784ba7d9b3
surajkumar345678 <kumarsuraj345678@gmail.com>
2025-01-14 18:24:15 +0530

    Add search and filter functionality for countries with UI enhancements

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const countriesContainer = document.querySelector(".countries-container");
+const filterByRegion = document.querySelector(".filter-by-region");
+const searchInput = document.querySelector(".search-container input");
 
-fetch("https://restcountries.com/v3.1/all")
+let allCountriesData = fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
-    data.forEach((country) => {
-      // console.log(country);
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `/country.html?name=${country.name.common}`;
-      countryCard.innerHTML = `
+    renderCountries(data);
+    allCountriesData = data;
+  });
+
+filterByRegion.addEventListener("change", (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries);
+});
+
+function renderCountries(data) {
+  countriesContainer.innerHTML = "";
+  data.forEach((country) => {
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `/country.html?name=${country.name.common}`;
+    countryCard.innerHTML = `
             <img src="${country.flags.svg}" alt="${country.name.common} flag" />
             <div class="card-text">
                 <h3 class="card-title">${country.name.common}</h3>
@@ -19,6 +32,13 @@ fetch("https://restcountries.com/v3.1/all")
                 <p><b>Capital: </b>${country.capital?.[0]}</p>
             </div>
     `;
-      countriesContainer.append(countryCard);
-    });
+    countriesContainer.append(countryCard);
   });
+}
+
+searchInput.addEventListener("input", (e) => {
+  const filteredCountries = allCountriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  renderCountries(filteredCountries);
+});
