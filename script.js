6e4a957819e58e7e8d857eee809eb4706bff9b7b
surajkumar345678 <kumarsuraj345678@gmail.com>
2025-01-17 14:42:32 +0530

    Refactor country card rendering and improve dark mode toggle functionality

--- script.js
+++ script.js
@@ -25,16 +25,11 @@ function renderCountries(data) {
     const countryCard = document.createElement("a");
     countryCard.classList.add("country-card");
     countryCard.href = `https://surajkumar345678.github.io/REST-Countries-API-Project/country.html?name=${country.name.common}`;
-
     countryCard.innerHTML = `
-      <img src="${country.flags.svg}" alt="${
-      country.name.common
-    } flag" loading="lazy" />
+      <img src="${country.flags.svg}" alt="${country.name.common} flag" loading="lazy" />
       <div class="card-text">
         <h3 class="card-title">${country.name.common}</h3>
-        <p><b>Population: </b>${Intl.NumberFormat("en-IN").format(
-          country.population
-        )}</p>
+        <p><b>Population: </b>${Intl.NumberFormat("en-IN").format(country.population)}</p>
         <p><b>Region: </b>${country.region}</p>
         <p><b>Capital: </b>${country.capital?.[0]}</p>
       </div>
@@ -55,7 +50,7 @@ searchInput.addEventListener("input", (e) => {
 modeSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   const isDarkMode = document.body.classList.contains("dark");
-
+  
   // Update the toggle text
   modeSwitch.innerHTML = isDarkMode
     ? '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Light Mode'
@@ -70,11 +65,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
     document.body.classList.add("dark");
-    modeSwitch.innerHTML =
-      '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Light Mode';
+    modeSwitch.innerHTML = '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Light Mode';
   } else {
     document.body.classList.remove("dark");
-    modeSwitch.innerHTML =
-      '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode';
+    modeSwitch.innerHTML = '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode';
   }
 });
